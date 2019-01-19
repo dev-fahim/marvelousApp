59a2138b5d52af4e42688647b55d5e3549bb944d
Fahim Al Islam <34773501+dev-fahim@users.noreply.github.com>
2019-01-20 02:02:42 +0600

    added validation in expend record adding feature
    if enough fund is not available the entry will not be accepted

--- src/backend/expenditure/api/serializers.py
+++ src/backend/expenditure/api/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from expenditure.models import ExpenditureHeadingModel, ExpenditureRecordModel
+from credit.models import CreditFundModel
 from base_user.models import BaseUserModel
 from sub_user.models import SubUserModel
+from utils import utils
 import uuid
 
 
@@ -53,6 +55,32 @@ class ExpenditureRecordModelSerializer(serializers.ModelSerializer):
 
     def logged_in_user(self):
         return self.request_data().user
+    
+    def validate_amount(self, value):
+        base_user = BaseUserModel.objects.filter(base_user=self.logged_in_user()) 
+        sub_user = SubUserModel.objects.filter(root_user=self.logged_in_user())
+        expend_obj = []
+
+        if base_user.exists() is True:
+            expend_obj = ExpenditureRecordModel.objects.filter(base_user=self.logged_in_user().base_user)
+            credit_fund_obj = CreditFundModel.objects.filter(base_user=self.logged_in_user().base_user)
+        elif sub_user.exists() is True:
+            base_user = sub_user.base_user
+            expend_obj = ExpenditureRecordModel.objects.filter(base_user=base_user)
+            credit_fund_obj = CreditFundModel.objects.filter(base_user=base_user)
+        
+        all_credit_fund_amounts = [obj.amount for obj in credit_fund_obj]
+        all_record_amounts = [obj.amount for obj in expend_obj]
+
+        total_pre_credit_fund_amount = utils.sum_int_of_array(all_credit_fund_amounts)
+        total_pre_record_amount = utils.sum_int_of_array(all_record_amounts)
+
+        record_value_after_entry = total_pre_record_amount + value
+        credit_fund_value_after_entry = total_pre_credit_fund_amount - record_value_after_entry
+
+        if credit_fund_value_after_entry >= 0:
+            return value
+        raise serializers.ValidationError(detail='Credit Fund will be exceede! So you cannot add any more records. After authority add more Credit Fund in Database you can entry more records.')
 
     def create(self, validated_data):
         if BaseUserModel.objects.filter(base_user=self.logged_in_user()).exists():
