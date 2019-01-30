27bebe3535769fa27b31267e194c91c001157804
Fahim Al Islam <34773501+dev-fahim@users.noreply.github.com>
2019-01-30 22:22:16 +0600

    Updated server url

--- src/frontend/project/src/app/service/server.url.ts
+++ src/frontend/project/src/app/service/server.url.ts
@@ -1,3 +1,3 @@
 
-export const LOCAL_REST_API_SERVER = 'http://192.168.1.239:8000/api/';
-export const LOCAL_SERVER = 'http://192.168.1.239:8000/';
+export const LOCAL_REST_API_SERVER = 'http://103.110.217.173:8000/api/';
+export const LOCAL_SERVER = 'http://103.110.217.173:8000/';
